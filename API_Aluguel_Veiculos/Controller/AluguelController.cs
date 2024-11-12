edc1a3b4bc8ac88a2e25ac6cc16291de0beecaae
leonardo <leo-nino70@outlook.com>
2024-11-12 19:46:34 -0300

    CRUD_Files

---
+++ API_Aluguel_Veiculos/Controller/AluguelController.cs
@@ -0,0 +1,114 @@
+﻿using API_Aluguel_de_carros.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Aluguel_de_carros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AluguelController : ControllerBase
+    {
+        private static List<Carro> carros = new List<Carro>();
+
+        [HttpPost]
+        public ActionResult<List<Carro>>
+            AddCarro(Carro newCar)
+        {
+
+            if (newCar.Id == 0)
+            {
+                newCar.alugado = false;
+                newCar.Id = (carros.Count == 0) ? 1 : carros[carros.Count - 1].Id + 1;
+            }
+
+            carros.Add(newCar);
+
+            return Ok(carros);
+        }
+
+
+        [HttpPut("{id_modelo}")]
+        public ActionResult<List<Carro>>
+            AlugarCarro(String id_modelo)
+        {
+            Carro carroAluguel = null;
+
+            if (int.TryParse(id_modelo, out int id)) {
+                carroAluguel = carros.Find(x => x.Id == id);
+            }
+
+            if (carroAluguel == null)
+            {
+                carroAluguel = carros.Find(x => x.modelo == id_modelo);
+            }
+
+            if (carroAluguel != null)
+            {
+                carroAluguel.alugado = true;
+                return Ok(carroAluguel);
+            }
+
+            return NotFound("Não foi buscar por modelo ou id");
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<Carro>>
+        MostrarTodosOsCarros()
+        {
+            return Ok(carros);
+
+        }
+
+        [HttpGet("find")]
+        public ActionResult<List<Carro>>
+     MostrarAlugado([FromQuery] string id_modelo)
+        {
+            Carro carroAluguel = null;
+
+            if (int.TryParse(id_modelo, out int id))
+            {
+                carroAluguel = carros.Find(x => x.Id == id);
+            }
+
+            if (carroAluguel == null)
+            {
+                carroAluguel = carros.Find(x => x.modelo == id_modelo);
+            }
+
+            if (carroAluguel != null)
+            {
+                return Ok(carroAluguel.alugado);
+            }
+
+            return NotFound("Não foi buscar por modelo ou id");
+        }
+
+        [HttpPut("/api/Aluguel/devolucao/{id_modelo}")]
+        public ActionResult<List<Carro>>
+          DevolucaoCarro(String id_modelo)
+        {
+            Carro carroAluguel = null;
+
+            if (int.TryParse(id_modelo, out int id))
+            {
+                carroAluguel = carros.Find(x => x.Id == id);
+            }
+
+            if (carroAluguel == null)
+            {
+                carroAluguel = carros.Find(x => x.modelo == id_modelo);
+            }
+
+            if (carroAluguel != null)
+            {
+                carroAluguel.alugado = false;
+                return Ok(carroAluguel);
+            }
+
+            return NotFound("Não foi buscar por modelo ou id");
+
+        }
+
+    }
+}
---
+++ API_Aluguel_Veiculos/Model/Carro.cs
@@ -0,0 +1,13 @@
+﻿namespace API_Aluguel_de_carros.Models
+{
+    public class Carro
+    {
+        public int Id { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public int ano { get; set; }
+        public string cor { get; set; }
+        public Boolean alugado { get; set; }
+        
+    }
+}
